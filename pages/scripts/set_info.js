0703df423dce94ca6f8c26b741c22116c42d7053
yyyeying <henrybai678121@163.com>
2023-01-07 15:06:09 -0500

    add update comment

--- pages/scripts/set_info.js
+++ pages/scripts/set_info.js
@@ -3,6 +3,18 @@ var setInfo = (info) => {
     document.getElementById("year").innerHTML = info.time.year;
     document.getElementById("month").innerHTML = info.time.month;
     document.getElementById("day").innerHTML = info.time.day;
+    if (info.comment) {
+        var commentHTML = "<ul>";
+        for (let index = 0; index < info.comment.length; index++) {
+            const comment = info.comment[index];
+            commentHTML += "<li><span class=\"text\">" + comment + "</span></li>";
+        }
+        commentHTML += "</ul>";
+        document.getElementById("comment").innerHTML = commentHTML;
+    } else {
+        document.getElementById("comment").style.display = "none";
+        document.getElementById("comment-title").style.display = "none";
+    }
 }
 
 export {setInfo}
